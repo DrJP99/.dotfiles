7e31c11ab87f66bf4abd89a2c0f7ce3d3610707c
DrJP99 <jpgzz1999@gmail.com>
2024-05-28 15:04:34 -0600

    added i3, dunst and polybar

---
+++ .config/i3/polybar.sh
@@ -0,0 +1,10 @@
+#!/usr/bin
+
+# Terminate running polybars
+# killall -q polybar
+
+# Wait until they've shut down
+# while grep -x polybar >/dev/null; do sleep 1; done
+
+# Launch polybar
+polybar example
